96a201fa37b301be5e662ac06698f1b5c053a7eb
Paulo Corazza <paulo.corazza@innovareti.com.br>
2019-01-07 00:31:01 -0200

    tratativa de erro

--- app/src/main/java/com/example/paulinhocorazza/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/example/paulinhocorazza/alcoolougasolina/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,62 +29,66 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String textoPrecoAlcool = precoAlcool.getText().toString();
                 String textoPrecoGasolina = precoGasolina.getText().toString();
-
-                //Converte valores string para numeros
-                Double valorAlcool = Double.parseDouble( textoPrecoAlcool );
-                Double valorGasolina = Double.parseDouble( textoPrecoGasolina );
-
-                Double resultado = valorAlcool/valorGasolina;
-
-                if(resultado <= 0.7){
-                    alerta = new AlertDialog.Builder(MainActivity.this);
-                    alerta.setTitle("Aqui está o seu resultado !");
-                    alerta.setMessage("Nesse posto compensa abastecer com Alcool .");
-                    alerta.setCancelable(false);
-                    alerta.setIcon(android.R.drawable.ic_dialog_alert);
-                    alerta.setNegativeButton("",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    alerta.setCancelable(true);
-                                }
-                            });
-                    alerta.setPositiveButton("Ok, Recalcular ...",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            });
-                    alerta.create();
-                    alerta.show();
-
+                
+                if (precoGasolina.getText().toString().isEmpty() || precoAlcool.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this,"Dados nao preenchido", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    alerta = new AlertDialog.Builder(MainActivity.this);
-                    alerta.setTitle("Aqui está o seu resultado !");
-                    alerta.setMessage("Nesse posto compensa abastecer com Gasolina .");
-                    alerta.setCancelable(false);
-                    alerta.setIcon(android.R.drawable.ic_dialog_alert);
-                    alerta.setNegativeButton("",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    alerta.setCancelable(true);
-                                }
-                            });
-                    alerta.setPositiveButton("Ok, Recalcular ...",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            });
-                    alerta.create();
-                    alerta.show();
+                    //Converte valores string para numeros
+                    Double valorAlcool = Double.parseDouble( textoPrecoAlcool );
+                    Double valorGasolina = Double.parseDouble( textoPrecoGasolina );
 
+                    Double resultado = valorAlcool/valorGasolina;
+                    if(resultado <= 0.7){
+                        alerta = new AlertDialog.Builder(MainActivity.this);
+                        alerta.setTitle("Aqui está o seu resultado !");
+                        alerta.setMessage("Nesse posto compensa abastecer com Alcool .");
+                        alerta.setCancelable(false);
+                        alerta.setIcon(android.R.drawable.ic_dialog_alert);
+                        alerta.setNegativeButton("",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        alerta.setCancelable(true);
+                                    }
+                                });
+                        alerta.setPositiveButton("Ok, Recalcular ...",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        alerta.create();
+                        alerta.show();
 
+                    }
+                    else{
+                        alerta = new AlertDialog.Builder(MainActivity.this);
+                        alerta.setTitle("Aqui está o seu resultado !");
+                        alerta.setMessage("Nesse posto compensa abastecer com Gasolina .");
+                        alerta.setCancelable(false);
+                        alerta.setIcon(android.R.drawable.ic_dialog_alert);
+                        alerta.setNegativeButton("",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        alerta.setCancelable(true);
+                                    }
+                                });
+                        alerta.setPositiveButton("Ok, Recalcular ...",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        alerta.create();
+                        alerta.show();
+
+                    }
                 }
+
             }
         });
 
