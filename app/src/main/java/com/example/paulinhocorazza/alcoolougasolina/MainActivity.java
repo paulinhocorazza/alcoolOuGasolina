70fdd947edb7b28ec48f9dc8045e38af3dd4192a
Paulo Corazza <corazza.paulovinicius@gmail>
2019-01-06 22:06:00 -0200

    app alcool ou gasolina primeira versao

---
+++ app/src/main/java/com/example/paulinhocorazza/alcoolougasolina/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.paulinhocorazza.alcoolougasolina;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText precoGasolina;
+    private EditText precoAlcool;
+    private AlertDialog.Builder alerta;
+    private Button btnBVerifica;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        precoGasolina = (EditText) findViewById(R.id.inputGasolina);
+        precoAlcool = (EditText) findViewById(R.id.inputAlcool);
+        btnBVerifica = (Button) findViewById(R.id.btnVerifica);
+        btnBVerifica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoPrecoAlcool = precoAlcool.getText().toString();
+                String textoPrecoGasolina = precoGasolina.getText().toString();
+
+                //Converte valores string para numeros
+                Double valorAlcool = Double.parseDouble( textoPrecoAlcool );
+                Double valorGasolina = Double.parseDouble( textoPrecoGasolina );
+
+                Double resultado = valorAlcool/valorGasolina;
+
+                if(resultado <= 0.7){
+                    alerta = new AlertDialog.Builder(MainActivity.this);
+                    alerta.setTitle("Aqui está o seu resultado !");
+                    alerta.setMessage("Nesse posto compensa abastecer com Alcool .");
+                    alerta.setCancelable(false);
+                    alerta.setIcon(android.R.drawable.ic_dialog_alert);
+                    alerta.setNegativeButton("",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    alerta.setCancelable(true);
+                                }
+                            });
+                    alerta.setPositiveButton("Ok, Recalcular ...",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                    alerta.create();
+                    alerta.show();
+
+                }
+                else{
+                    alerta = new AlertDialog.Builder(MainActivity.this);
+                    alerta.setTitle("Aqui está o seu resultado !");
+                    alerta.setMessage("Nesse posto compensa abastecer com Gasolina .");
+                    alerta.setCancelable(false);
+                    alerta.setIcon(android.R.drawable.ic_dialog_alert);
+                    alerta.setNegativeButton("",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    alerta.setCancelable(true);
+                                }
+                            });
+                    alerta.setPositiveButton("Ok, Recalcular ...",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                    alerta.create();
+                    alerta.show();
+
+
+                }
+            }
+        });
+
+    }
+}
