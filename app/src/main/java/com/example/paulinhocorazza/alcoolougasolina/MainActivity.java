0d53892bf98415662da8765ec21852a4652ce357
Paulo Corazza <corazza.paulovinicius@gmail>
2019-01-08 02:22:22 -0200

    primeira versão play store

--- app/src/main/java/com/example/paulinhocorazza/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/example/paulinhocorazza/alcoolougasolina/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.paulinhocorazza.alcoolougasolina;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText precoAlcool;
     private AlertDialog.Builder alerta;
     private Button btnBVerifica;
+    private ImageView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,40 @@ public class MainActivity extends AppCompatActivity {
 
         precoGasolina = (EditText) findViewById(R.id.inputGasolina);
         precoAlcool = (EditText) findViewById(R.id.inputAlcool);
+        info = (ImageView) findViewById(R.id.infoId);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alerta = new AlertDialog.Builder(MainActivity.this);
+                alerta.setMessage("Fale com o desenvolvedor");
+                alerta.setCancelable(false);
+                alerta.setIcon(android.R.drawable.ic_dialog_email);
+                alerta.setNegativeButton("Não, talvez mais tarde",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                alerta.setCancelable(true);
+                            }
+                        });
+                alerta.setPositiveButton("Sim, vamos mandar um email !",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent=new Intent(Intent.ACTION_SEND);
+                                String[] recipients={"corazza.paulovinicius@gmail.com"};
+                                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                                intent.putExtra(Intent.EXTRA_SUBJECT,"Aplicativo GASOSA OU ALCOOL?");
+                                intent.putExtra(Intent.EXTRA_TEXT,"");
+                                intent.putExtra(Intent.EXTRA_CC,"corazza.paulovinicius@gmail.com");
+                                intent.setType("text/html");
+                                intent.setPackage("com.google.android.gm");
+                                startActivity(Intent.createChooser(intent, "Send mail"));
+                            }
+                        });
+                alerta.create();
+                alerta.show();
+            }
+        });
         btnBVerifica = (Button) findViewById(R.id.btnVerifica);
         btnBVerifica.setOnClickListener(new View.OnClickListener() {
             @Override
